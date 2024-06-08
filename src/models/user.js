a3417e9db2200551ddc71d6a741c26e573531be6
54JIN <sajinsaju2002@gmail.com>
2024-06-08 19:46:28 -0400

    Initial

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(val) {
+            if(!validator.isEmail(val)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+    }
+})
+
+module.exports = User
